007aefc636821a9cdb0b98f49504b32960b30e95
Despina <despathz@gmail.com>
2016-01-10 20:10:35 +0200

    updated Main

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,58 @@
+import java.util.*;
+
 import gr.demokritos.iit.jinsect.utils;
 import gr.demokritos.iit.jinsect.documentModel.representations.DocumentNGramGraph;
+import gr.demokritos.iit.jinsect.structs.UniqueVertexGraph;
 
 public class Main 
 {
-
-    public static void main(String[] args) 
+    public static void main(String[] args) throws Exception 
     {
-    	 // The string we want to represent
-	   	 String sTmp = "New graph!";
+    	// The string we want to represent
+	   	String sTmp = "abcd";
 	
-	   	 // The default document n-gram graph, with min n-gram size and max n-gram size set to 3, and the dist parameter set to 3.
-	   	DocumentNGramGraph dngGraph = new DocumentNGramGraph(); 
+	   	 // Min n-gram size and max n-gram size set to 1, and the dist parameter set to 1.
+	   	DocumentNGramGraph dngGraph = new DocumentNGramGraph(1, 1, 1); 
 	
 	   	// Create the graph
 	   	dngGraph.setDataString(sTmp);
 	   	
+	   	int total_vertices = dngGraph.getGraphLevel(0).UniqueVertices.values().size(); //total vertices
+	    System.out.println(total_vertices);	    
+	    int total_edges = dngGraph.length(); //total edges
+	    System.out.println(total_edges); 
+	    
+	    Set <String> setEdges =  dngGraph.getGraphLevel(0).getEdgeSet(); //set of edges
+	    System.out.println(setEdges); 
+	    
+	    Set <String> setVetrices = dngGraph.getGraphLevel(0).UniqueVertices.keySet(); //set of vertices
+	    System.out.println(setVetrices);
+	    
+	    /*Local Search Algorithm
+	     * Input: 
+	     * 		- V: the set of variables  
+	     * 		- dom: the domain for each variable
+	     * 		- C: set of constraints
+	     * Output: 
+	     * 		A complete assignment
+	     * Extra:
+	     * 		- arrLoc[V] will be a local array of values indexed by V
+	     */
+	    
+//	    String[] arrLoc = new String[total_vertices]; 
+//	    while (true)
+//	    {
+//	    	for(int i = 0; i < total_vertices; i++)
+//	    	{
+//	    		
+//	    	}
+//	    }
+	    
+	    
 	   	/* The following command gets the first n-gram graph level (with the minimum n-gram
         size) and renders it, using the utils package, as a DOT string */
-        System.out.println(utils.graphToDot(dngGraph.getGraphLevel(0), true));
-	   	
-	   	
+        System.out.println(utils.graphToDot(dngGraph.getGraphLevel(0), true));	
+        
     }
 }
 
