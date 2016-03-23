f122ff4c0a9a7010f63ae50e04b94cd13596eb8f
Despina Athanasia Pantazi <despathz@gmail.com>
2016-03-23 13:52:26 +0200

    Updated BFS with methodCost

--- src/BFSSearchAlgorithm.java
+++ src/BFSSearchAlgorithm.java
@@ -3,7 +3,13 @@ import java.util.List;
 
 public class BFSSearchAlgorithm implements ISearchAlgorithm<String>
 {
-
+	int methodCost;
+	
+	public BFSSearchAlgorithm()
+	{
+		methodCost = 0;
+	}
+	
 	@Override
 	public IProblemTreeNode<String> getSolutionFor(IProblem<String> pToSolve)
 	{
@@ -16,12 +22,14 @@ public class BFSSearchAlgorithm implements ISearchAlgorithm<String>
 	        
 	        // Initialize list of possible nodes
 	        List<IProblemTreeNode<String>> lNextCandidates = new ArrayList<>();
+	        methodCost++; //add cost of the lNextCandidates cost
 	        lNextCandidates.add(ptnCurrent);
 	        
 	        // If we have done less than 10K steps and
 	        // we have not yet found a solution
 	        while ((iCnt++ < 1000000) && (!pToSolve.isSolution(ptnCurrent))) 
 	        {
+	       	 methodCost++; //isSolution() method cost
 	       	 System.out.println("in");
 	       	 // If we checked all cases
 	       	 if (lNextCandidates.size() == 0)
@@ -36,6 +44,7 @@ public class BFSSearchAlgorithm implements ISearchAlgorithm<String>
 	       	 lNextCandidates.remove(0);
 	            
 	       	 // If the node is valid
+	       	 methodCost++; //isValid() method cost
 	       	 if (pToSolve.isValid(ptnCurrent))
 	       		 // Add its children to the possible next steps
 	       		 lNextCandidates.addAll(pToSolve.getNextStatesFor(ptnCurrent));
@@ -43,10 +52,16 @@ public class BFSSearchAlgorithm implements ISearchAlgorithm<String>
 	        }
 	        
 	        // If we have a solution
+	        methodCost++; //isSolution() method cost
 	        if (pToSolve.isSolution(ptnCurrent))
 	            return ptnCurrent; // Return it
 	        else
 	            return null; // Nothing found 
+	}
+	
+	public Integer getMethodCost()
+	{
+		return methodCost;
 	}
 
 }
